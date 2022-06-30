7c5c647d78b1136ba7dc70cf476785aa9acf8f52
DayeonJung <ekdus0227@gmail.com>
2022-06-30 18:44:54 +0900

    feat: FinanceHomeVC 하위에 child VC 추가하기
    - interactor에서 router에게 attach하도록 호출
    - router에서 view(controller)의 addDashboard 메서드 호출
    - FinanceHomeVC에서 실제 addChild 메서드 호출

--- MiniSuperApp/FinanceHome/FinanceHomeRouter.swift
+++ MiniSuperApp/FinanceHome/FinanceHomeRouter.swift
@@ -1,18 +1,20 @@
 import ModernRIBs
 
-protocol FinanceHomeInteractable: Interactable {
+protocol FinanceHomeInteractable: Interactable, SuperPayDashboardListener {
   var router: FinanceHomeRouting? { get set }
   var listener: FinanceHomeListener? { get set }
 }
 
 protocol FinanceHomeViewControllable: ViewControllable {
-  // TODO: Declare methods the router invokes to manipulate the view hierarchy.
+  func addDashboard(_ view: ViewControllable)
 }
 
 final class FinanceHomeRouter: ViewableRouter<FinanceHomeInteractable, FinanceHomeViewControllable>, FinanceHomeRouting {
   
     private let superPayDashboardBuildable: SuperPayDashboardBuildable
     
+  private var superPayRouting: Routing?
+  
   // TODO: Constructor inject child builder protocols to allow building children.
   init(
     interactor: FinanceHomeInteractable,
@@ -23,5 +25,18 @@ final class FinanceHomeRouter: ViewableRouter<FinanceHomeInteractable, FinanceHo
     
     super.init(interactor: interactor, viewController: viewController)
     interactor.router = self
+  }
+  
+  func attachSuperPayDashboard() {
+    if superPayRouting != nil {
+      return
+    }
+    
+    let router = superPayDashboardBuildable.build(withListener: interactor)
+    let dashboard = router.viewControllable
+    viewController.addDashboard(dashboard)
+    
+    self.superPayRouting = router
+    attachChild(router)
   }
 }
