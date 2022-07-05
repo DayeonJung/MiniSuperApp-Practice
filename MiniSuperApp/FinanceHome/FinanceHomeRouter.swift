050263d47c7cf45d5d19b926e5ae34593eada929
DayeonJung <ekdus0227@gmail.com>
2022-07-05 14:23:37 +0900

    feat: FinanceHome RIB 하위에 CardOnFileDashboard RIB을 attach
    1. FinanceHomeBuilder의 build 메서드에서, cardOnFileDashboardBuilder를 생성하여 FinanceHomeRouter 초기화에 추가
    2. FinanceHomeRouter에 cardOnFileDashboardBuildable, cardOnFileRouting 변수 추가
    3. FinanceHomeRouting에 attachCardOnFileDashboard 메서드 추가
    4. FinanceHomeRouter에 attachCardOnFileDashboard 메서드 정의

--- MiniSuperApp/FinanceHome/FinanceHomeRouter.swift
+++ MiniSuperApp/FinanceHome/FinanceHomeRouter.swift
@@ -1,6 +1,8 @@
 import ModernRIBs
 
-protocol FinanceHomeInteractable: Interactable, SuperPayDashboardListener {
+protocol FinanceHomeInteractable: Interactable,
+                                  SuperPayDashboardListener,
+                                  CardOnFileDashboardListener {
   var router: FinanceHomeRouting? { get set }
   var listener: FinanceHomeListener? { get set }
 }
@@ -12,16 +14,20 @@ protocol FinanceHomeViewControllable: ViewControllable {
 final class FinanceHomeRouter: ViewableRouter<FinanceHomeInteractable, FinanceHomeViewControllable>, FinanceHomeRouting {
   
     private let superPayDashboardBuildable: SuperPayDashboardBuildable
-    
   private var superPayRouting: Routing?
+  
+  private let cardOnFileDashboardBuildable: CardOnFileDashboardBuildable
+  private var cardOnFileRouting: Routing?
   
   // TODO: Constructor inject child builder protocols to allow building children.
   init(
     interactor: FinanceHomeInteractable,
     viewController: FinanceHomeViewControllable,
-    superPayDashboardBuildable: SuperPayDashboardBuildable
+    superPayDashboardBuildable: SuperPayDashboardBuildable,
+    cardOnFileDashboardBuildable: CardOnFileDashboardBuildable
   ) {
     self.superPayDashboardBuildable = superPayDashboardBuildable
+    self.cardOnFileDashboardBuildable = cardOnFileDashboardBuildable
     
     super.init(interactor: interactor, viewController: viewController)
     interactor.router = self
@@ -37,6 +43,19 @@ final class FinanceHomeRouter: ViewableRouter<FinanceHomeInteractable, FinanceHo
     viewController.addDashboard(dashboard)
     
     self.superPayRouting = router
+    attachChild(router)
+  }
+  
+  func attachCardOnFileDashboard() {
+    if cardOnFileRouting != nil {
+      return
+    }
+    
+    let router = cardOnFileDashboardBuildable.build(withListener: interactor)
+    let dashboard = router.viewControllable
+    viewController.addDashboard(dashboard)
+    
+    self.cardOnFileRouting = router
     attachChild(router)
   }
 }
