a87a1752264f45f5ac8593620d52ef42a2d15f29
DayeonJung <ekdus0227@gmail.com>
2022-07-05 15:42:11 +0900

    feat: 슈퍼페이 홈 - 카드 및 계좌에 보여줄 UI 생성
    - AddPaymentMethodButton 생성 (카드 추가 버튼)
    - PaymentMethodView 생성 (title, subtitle로 구성)

---
+++ MiniSuperApp/FinanceHome/CardOnFileDashboard/Views/AddPaymentMethodButton.swift
@@ -0,0 +1,43 @@
+//
+//  AddPaymentMethodButton.swift
+//  MiniSuperApp
+//
+//  Created by Dayeon Jung on 2022/07/05.
+//
+
+import UIKit
+
+final class AddPaymentMethodButton: UIControl {
+  private let plusIcon: UIImageView = {
+    let imageView = UIImageView(
+      image: UIImage(
+        systemName: "plus",
+        withConfiguration: UIImage.SymbolConfiguration(pointSize: 24, weight: .semibold)
+      )
+    )
+    imageView.tintColor = .white
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    return imageView
+  }()
+  
+  init() {
+    super.init(frame: .zero)
+    
+    setupViews()
+  }
+  
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    
+    setupViews()
+  }
+  
+  private func setupViews() {
+    addSubview(plusIcon)
+    
+    NSLayoutConstraint.activate([
+      plusIcon.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+      plusIcon.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+    ])
+  }
+}
